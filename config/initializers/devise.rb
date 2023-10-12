4a2313a27ffc8f9354fb3330e3b7271e917358ba
rod <rodrip0129@gmail.com>
2023-10-12 14:30:19 -0300

    improve styles and fix linters

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -302,7 +302,7 @@ Devise.setup do |config|
   # Note: These might become the new default in future versions of Devise.
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
-  
+
   config.scoped_views = true
 
   # ==> Configuration for :registerable
