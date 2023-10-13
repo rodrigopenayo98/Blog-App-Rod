f1c14a1aec024dbcf83f1416bffab163c89fc6f1
rod <rodrip0129@gmail.com>
2023-10-13 15:01:55 -0300

    add api endpoint for list comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :find_post, only: %i[index create]
+
+  def index
+    @comments = @post.comments
+    render json: @comments
+  end
+
+  def create
+    @comment = @post.comments.new(comment_paramas)
+    @comment.user = current_user
+    if @comment.save
+      render json: @comment, status: :created
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+  def find_post
+    @post = Post.find(params[:post_id])
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_default_response_format, if: :api_request?
 
   def after_sign_in_path_for(_resource)
     users_path
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def api_request?
+    request.path.start_with?('/api/')
+  end
+
+  def set_default_response_format
+    request.format = :json
   end
 end
