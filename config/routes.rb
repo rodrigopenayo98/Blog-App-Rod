996bbb5abdb08431f48d7fac36c3643727567ca5
rod <rodrip0129@gmail.com>
2023-10-13 14:21:07 -0300

    generate controllr for api/v1/posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :show] do
-        resources :posts, only: [:index, :show] do
-          resources :comments, only: [:create, :destroy]
+      resources :users, only: %i[index show] do
+        resources :posts, only: %i[index show] do
+          resources :comments, only: %i[create destroy]
           resources :likes, only: [:create]
         end
       end
